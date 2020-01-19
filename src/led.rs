20e9dcd4888fd1260b9b0d0827fe206ba43d72dc
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2020-01-18 22:35:52 -0700

    Test LEDs

--- src/led.rs
+++ src/led.rs
@@ -1,4 +1,14 @@
 extern crate stm32l0xx_hal;
-use stm32l0xx_hal::{pac, prelude::*};
+use stm32l0xx_hal::{pac, prelude::*, rcc::Config};
 
-let led_1, led_2 = PD<Output<PushPull>>;
+pub fn led_test() -> () {
+    let periph = pac::Peripherals::take().unwrap();
+    let mut rcc = periph.RCC.freeze(Config::hsi16());
+
+    let gpioa = periph.GPIOA.split(&mut rcc);
+
+    let mut led1 = gpioa.pa0.into_push_pull_output();
+    let mut led2 = gpioa.pa1.into_push_pull_output();
+    led1.set_high().unwrap();
+    led2.set_high().unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
 use cortex_m_rt::entry;
-//use cortex_m_semihosting::{hprintln};
+use cortex_m_semihosting::{hprintln};
+
+mod led;
+use led::led_test;
 
 #[entry]
 fn main() -> ! {
-    //hprintln!("Hello, world!").unwrap();
-
+led_test();
     loop {
         // your code goes here
     }
