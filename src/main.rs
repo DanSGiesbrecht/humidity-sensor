b7666bb0a672564f71078acc2c5c8ad4e2599edb
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2020-02-15 12:18:03 -0700

    Test I2C (PA9/10)
    - I2C1 peripheral (I2C2 not set up)
    - PA9/PA10
    - stm32l0xx-hal submodule needed, AF different for PA9/PA10

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_halt;
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{hprintln};
+extern crate shtcx;
 
-mod led;
-use led::led_test;
+// use patch
+use stm32l0xx_hal::{pac, prelude::*, rcc::Config};
 
 #[entry]
 fn main() -> ! {
-led_test();
+    let periph = pac::Peripherals::take().unwrap();
+
+    let mut rcc = periph.RCC.freeze(Config::hsi16());
+    let gpioa = periph.GPIOA.split(&mut rcc);
+
+    let sda = gpioa.pa10.into_open_drain_output();
+    let scl = gpioa.pa9.into_open_drain_output();
+
+    let mut i2c = periph.I2C1.i2c(sda, scl, 10.khz(), &mut rcc);
+
+    let mut buffer = [0u8; 2];
+    const ADDRESS: u8 = 0x70;
+
+    i2c.write(ADDRESS, &mut buffer).unwrap();
+
     loop {
-        // your code goes here
     }
 }
+
