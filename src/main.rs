eb009bfe3c0d65592e0baab066dcb15a75a7cff6
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2020-02-23 15:56:59 -0700

    Test SHTC3 sensor - normal mode
    - Tested reading temp & humidity (temp-first)

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@
 #![no_main]
 
 extern crate panic_halt;
-
-use cortex_m_rt::entry;
 extern crate shtcx;
 
+use cortex_m_rt::entry;
+
 // use patch
-use stm32l0xx_hal::{pac, prelude::*, rcc::Config};
+use stm32l0xx_hal::{pac, prelude::*, rcc::Config, delay::Delay};
+
+use shtcx::{ShtCx, PowerMode};
 
 #[entry]
 fn main() -> ! {
     let periph = pac::Peripherals::take().unwrap();
+    let core_periph = pac::CorePeripherals::take().unwrap();
 
     let mut rcc = periph.RCC.freeze(Config::hsi16());
     let gpiob = periph.GPIOB.split(&mut rcc);
@@ -19,14 +22,20 @@ fn main() -> ! {
     let sda = gpiob.pb14.into_open_drain_output();
     let scl = gpiob.pb13.into_open_drain_output();
 
-    let mut i2c = periph.I2C2.i2c(sda, scl, 10.khz(), &mut rcc);
+    let mut i2c = periph.I2C2.i2c(sda, scl, 100.khz(), &mut rcc);
+    let delay = Delay::new(core_periph.SYST, rcc.clocks);
 
-    let mut buffer = [0u8; 2];
     const ADDRESS: u8 = 0x70;
+    let mut sht = ShtCx::new(i2c, ADDRESS, delay);
 
-    i2c.write(ADDRESS, &mut buffer).unwrap();
+    sht.wakeup().unwrap();
+
+    for _ in 0..3 {
+        let measurement = sht.measure(PowerMode::NormalMode).unwrap();
+        let temp = measurement.temperature.as_degrees_celsius();
+        let hum = measurement.humidity.as_percent();
+    }
 
     loop {
     }
 }
-
