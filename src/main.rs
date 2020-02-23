6c0be83de334dd0bb6ad1aeab6c1a01fe1c0e0b4
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2020-02-22 20:26:37 -0700

    Test I2C (PB13/14)

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,12 @@ fn main() -> ! {
     let periph = pac::Peripherals::take().unwrap();
 
     let mut rcc = periph.RCC.freeze(Config::hsi16());
-    let gpioa = periph.GPIOA.split(&mut rcc);
+    let gpiob = periph.GPIOB.split(&mut rcc);
 
-    let sda = gpioa.pa10.into_open_drain_output();
-    let scl = gpioa.pa9.into_open_drain_output();
+    let sda = gpiob.pb14.into_open_drain_output();
+    let scl = gpiob.pb13.into_open_drain_output();
 
-    let mut i2c = periph.I2C1.i2c(sda, scl, 10.khz(), &mut rcc);
+    let mut i2c = periph.I2C2.i2c(sda, scl, 10.khz(), &mut rcc);
 
     let mut buffer = [0u8; 2];
     const ADDRESS: u8 = 0x70;
