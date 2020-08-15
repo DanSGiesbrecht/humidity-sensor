06291c1694f8913d50b4642ea8772e0a98c4840b
Logan Hunter <loganhunter27@gmail.com>
2020-08-15 02:08:56 -0600

    Clean up humidity reading
    Cleaned up the code for reading humidity from the sensor, and
    added TODOs for all of the other steps required to get
    the humidity sent out

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,64 @@ extern crate shtcx;
 
 use cortex_m_rt::entry;
 
-// use patch
-use stm32l0xx_hal::{pac, prelude::*, rcc::Config, delay::Delay};
+use stm32l0xx_hal::{
+    gpio::{
+        gpiob::{PB13, PB14},
+        Output,
+        OpenDrain
+    },
+    pac::{
+        Peripherals,
+        CorePeripherals,
+        I2C2
+    },
+    i2c::I2c,
+    prelude::*,
+    rcc::Config,
+    delay::Delay
+};
 
-use shtcx::{ShtCx, PowerMode};
+use shtcx::{ShtC3, shtc3, PowerMode, Measurement, LowPower};
+
+type Sht = ShtC3<I2c<I2C2, PB14<Output<OpenDrain>>, PB13<Output<OpenDrain>>>>;
 
 #[entry]
 fn main() -> ! {
-    let periph = pac::Peripherals::take().unwrap();
-    let core_periph = pac::CorePeripherals::take().unwrap();
+    let periph = Peripherals::take().unwrap();
+    let core_periph = CorePeripherals::take().unwrap();
 
     let mut rcc = periph.RCC.freeze(Config::hsi16());
     let gpiob = periph.GPIOB.split(&mut rcc);
 
     let sda = gpiob.pb14.into_open_drain_output();
     let scl = gpiob.pb13.into_open_drain_output();
+    let i2c = periph.I2C2.i2c(sda, scl, 100.khz(), &mut rcc);
 
-    let mut i2c = periph.I2C2.i2c(sda, scl, 100.khz(), &mut rcc);
-    let delay = Delay::new(core_periph.SYST, rcc.clocks);
-
-    const ADDRESS: u8 = 0x70;
-    let mut sht = ShtCx::new(i2c, ADDRESS, delay);
-
-    sht.wakeup().unwrap();
-
-    for _ in 0..3 {
-        let measurement = sht.measure(PowerMode::NormalMode).unwrap();
-        let temp = measurement.temperature.as_degrees_celsius();
-        let hum = measurement.humidity.as_percent();
-    }
+    let mut delay = Delay::new(core_periph.SYST, rcc.clocks);
+    let mut sht = shtc3(i2c);
 
     loop {
+        // TODO: Generate sleep time randomly
+
+        delay.delay(2000.ms());
+
+        let _ = get_humidity_temperature_measurement(&mut sht, &mut delay);
+
+        // TODO: Turn on RF section
+        
+        // TODO: Sleep for requisite time for RF to get ready
+
+        // TODO: Load UART/DMA buffer from measurement
+        
+        // TODO: Sleep until buffer will be done sending
+        
+        // TODO: Turn off RF section
     }
+}
+
+fn get_humidity_temperature_measurement(sht: &mut Sht, delay: &mut Delay) -> Measurement {
+    sht.wakeup(delay).unwrap();
+    let measurement = sht.measure(PowerMode::LowPower, delay).unwrap();
+    sht.sleep().unwrap();
+    measurement
 }
