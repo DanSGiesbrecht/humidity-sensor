40436d75f99c77294c78cf0853a5bd64a9cd7f99
Logan Hunter <loganhunter27@gmail.com>
2020-08-16 18:26:27 -0600

    Add full RNG
    Add a full RNG for time delay generation; based only
    on the initial reading of the humidity and temperature.
    Also made the conversion to combined formats nicer
    for the measurement values

--- src/board_support/random_number_generator.rs
+++ src/board_support/random_number_generator.rs
@@ -1,12 +1,17 @@
-pub struct RandomNumberGenerator;
+use rand::prelude::*;
+
+pub struct RandomNumberGenerator {
+    rng: SmallRng
+}
 
 impl RandomNumberGenerator {
-    pub fn new() -> Self {
-        RandomNumberGenerator
+    pub fn new(seed: u64) -> Self {
+        RandomNumberGenerator{
+            rng: SmallRng::seed_from_u64(seed)
+        }
     }
 
-    pub fn get_random_number(&mut self) -> f32 {
-        // TODO Implement getting random values between 0 and 1
-        0.5
+    pub fn next(&mut self, range_bottom: u32, range_top: u32) -> u32 {
+        self.rng.next_u32() % (range_top - range_bottom) + range_bottom
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 extern crate panic_halt;
 
 use cortex_m_rt::entry;
-
 use stm32l0xx_hal::{
     pac::{
         Peripherals,
@@ -32,26 +31,36 @@ fn main() -> ! {
     let mut delay = Delay::new(core_periph.SYST, rcc.clocks);
     let mut humidity_sensor = HumiditySensor::new(gpiob.pb14, gpiob.pb13, periph.I2C2, &mut rcc);
     let mut disabled_transmitter = RfTransmitter::new();
-    let mut random_number_generator = RandomNumberGenerator::new();
+
+    let mut random_number_generator = RandomNumberGenerator::new(
+        humidity_sensor.read(&mut delay).to_combined_u64()
+    );
 
     loop {
-        delay.delay(((2000f32 * random_number_generator.get_random_number()) as u32).ms());
+        delay.delay(random_number_generator.next(500, 2000).ms());
 
         let measurement = humidity_sensor.read(&mut delay);
 
         let mut enabled_transmitter = disabled_transmitter.enable(&mut delay);
-        enabled_transmitter.send(&convert_measurement_to_array(measurement));
+        enabled_transmitter.send(&measurement.to_combined_array());
         disabled_transmitter = enabled_transmitter.disable(&mut delay);
     }
 }
 
-fn convert_measurement_to_array(measurement: shtcx::Measurement) -> [u8;8] {
-    let temperature = measurement.temperature.as_millidegrees_celsius();
-    let humidity = measurement.humidity.as_millipercent();
+trait MeasurementExt {
+    fn to_combined_array(&self) -> [u8; 8];
+    fn to_combined_u64(&self) -> u64;
+}
 
-    array_init::from_iter(
-        temperature.to_le_bytes().iter().cloned().chain(
-            humidity.to_le_bytes().iter().cloned()
-        )
-    ).unwrap()
+impl MeasurementExt for shtcx::Measurement {
+    fn to_combined_array(&self) -> [u8; 8] {
+        self.to_combined_u64().to_le_bytes()
+    }
+
+    fn to_combined_u64(&self) -> u64 {
+        let temperature = self.temperature.as_millidegrees_celsius();
+        let humidity = self.humidity.as_millipercent();
+
+        (temperature as u64) | ((humidity as u64) << 32)
+    }
 }
