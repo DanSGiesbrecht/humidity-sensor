c8d2a363f573a6390288802e45a3b391e39f8863
Logan Hunter <loganhunter27@gmail.com>
2020-08-17 01:02:48 -0600

    Clean up code style

--- src/board_support/rf_transmitter.rs
+++ src/board_support/rf_transmitter.rs
@@ -17,37 +17,42 @@ use stm32l0xx_hal::{
 use core::marker::PhantomData;
 
 pub struct RfTransmitter<ENABLED> {
-    _marker:        PhantomData<ENABLED>,
-    rf_enable:      PA6<Output<PushPull>>,
-    rf_spi:         spi::Spi<SPI1, (NoSck, NoMiso, PA7<Analog>)>
+    _marker:    PhantomData<ENABLED>,
+    enable:     PA6<Output<PushPull>>,
+    spi:        spi::Spi<SPI1, (NoSck, NoMiso, PA7<Analog>)>
 }
 
 pub struct Enabled;
 pub struct Disabled;
 
 impl RfTransmitter<Disabled> {
-    pub fn new<T: PinMode>(rf_enable_pin: PA6<T>, mosi: PA7<T>, spi_periph: SPI1, rcc: &mut Rcc) -> RfTransmitter<Disabled> {
-        let mut rf_enable_pin = rf_enable_pin.into_push_pull_output();
-        rf_enable_pin.set_low().unwrap();
-        let rf_spi = spi_periph.spi((spi::NoSck, spi::NoMiso, mosi.into_analog()), spi::MODE_0, 50_000.hz(), rcc);
-
-        RfTransmitter{
+    pub fn new<T: PinMode>(enable_pin: PA6<T>, mosi: PA7<T>, spi_periph: SPI1, rcc: &mut Rcc) -> Self {
+        let mut transmitter = RfTransmitter{
             _marker:    PhantomData,
-            rf_enable:  rf_enable_pin,
-            rf_spi:     rf_spi
-        }
+            enable:     enable_pin.into_push_pull_output(),
+            spi:        spi_periph.spi(
+                (spi::NoSck, spi::NoMiso, mosi.into_analog()),
+                spi::MODE_0,
+                50_000.hz(),
+                rcc
+            )
+        };
+
+        transmitter.enable.set_low().unwrap();
+
+        transmitter
     }
 
     pub fn enable(mut self, delay: &mut Delay) -> RfTransmitter<Enabled> {
-        self.rf_enable.set_high().unwrap();
+        self.enable.set_high().unwrap();
 
         // TODO Determine correct amount of time to wait for stabilization
         delay.delay(100.ms());
 
         RfTransmitter{
             _marker:    PhantomData,
-            rf_enable:  self.rf_enable,
-            rf_spi:        self.rf_spi
+            enable:     self.enable,
+            spi:        self.spi
         }
     }
 }
@@ -58,17 +63,17 @@ impl RfTransmitter<Enabled> {
         // than just an arbitrary wait
         delay.delay(100.ms());
 
-        self.rf_enable.set_low().unwrap();
+        self.enable.set_low().unwrap();
 
         RfTransmitter{
             _marker:    PhantomData,
-            rf_enable:  self.rf_enable,
-            rf_spi:        self.rf_spi
+            enable:     self.enable,
+            spi:        self.spi
         }
     }
 
-    pub fn send(&mut self, _data: &[u8]) {
+    pub fn send(&mut self, data: &[u8]) {
         // TODO: Load transmit buffer and start transmission
-        self.rf_spi.write(_data).unwrap();
+        self.spi.write(data).unwrap();
     }
 }
